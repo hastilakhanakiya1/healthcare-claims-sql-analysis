66411ffb6471b474185f94b5bad1fc651ef23215
hastilakhanakiya1 <hastilakhanakiya1@gmail.com>
2026-09-25 17:17:53 -0400

    Update 02_claims_analysis.sql

--- 02_claims_analysis.sql
+++ 02_claims_analysis.sql
@@ -10,20 +10,18 @@ SELECT
     c.claim_id,
     m.member_name,
     c.service_date,
-    e.coverage_start,
-    e.coverage_end,
+    m.coverage_start,
+    m.coverage_end,
     c.claim_status,
     c.denial_reason,
     CASE
-        WHEN c.service_date BETWEEN e.coverage_start AND e.coverage_end
+        WHEN c.service_date BETWEEN m.coverage_start AND m.coverage_end
             THEN 'Eligible'
         ELSE 'Not Eligible'
     END AS eligibility_check
 FROM claims c
 JOIN members m
     ON c.member_id = m.member_id
-JOIN eligibility e
-    ON c.member_id = e.member_id
 ORDER BY c.claim_id;
 
 
@@ -117,20 +115,19 @@ ORDER BY denial_rate_percent DESC;
 
 -- 5. DATA QUALITY VALIDATION
 
-
 SELECT
     c.claim_id,
     m.member_name,
     p.provider_name,
     p.network_status,
     c.service_date,
-    e.coverage_end,
+    m.coverage_end,
     c.claim_status,
     c.denial_reason,
 
     CASE
         WHEN c.service_date NOT BETWEEN
-             e.coverage_start AND e.coverage_end
+             m.coverage_start AND m.coverage_end
              AND c.denial_reason != 'Member Not Eligible'
             THEN 'Review - Eligibility Mismatch'
 
@@ -148,8 +145,6 @@ SELECT
 FROM claims c
 JOIN members m
     ON c.member_id = m.member_id
-JOIN eligibility e
-    ON c.member_id = e.member_id
 JOIN providers p
     ON c.provider_id = p.provider_id
 
